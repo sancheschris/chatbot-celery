2058681eef40b282cd5b0054d0f5278744b59f37
Christian Sanches <christian.sanches-v@uplevelteam.com>
2026-03-13 17:01:15 -0300

    added chat interfce

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (e) => {
+    if (e.key === "Enter") {
+      setMessage("");
+      setMessages([...messages, { content: message, role: "user" }]);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wrapper">
+      <div className="chat-wrapper">
+        <div className="chat-history">
+          <div>
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={`message ${message.role === "user" ? " user" : ""}`}
+              >
+                {message.role === "user" ? "Me: " : "AI: "}
+                {message.content}
+              </div>
+            ))}
+          </div>
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyUp={sendMessage}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
